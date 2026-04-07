68445095f5dd5945995232c135af012ea9b23387
sincerelyindra <168695731+sincerelyindra@users.noreply.github.com>
2026-04-08 03:28:09 +0530

    Add baseline compatibility module

---
+++ src/robust_pricing/baselines.py
@@ -0,0 +1,7 @@
+from .core import _legacy
+
+fit_linear = _legacy.fit_linear
+fit_quadratic = _legacy.fit_quadratic
+fit_logit = _legacy.fit_logit
+
+__all__ = ['fit_linear', 'fit_quadratic', 'fit_logit']
