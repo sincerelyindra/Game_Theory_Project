07b380fad2285eb2a23b9c11cafe8ced73b49a9a
sincerelyindra <168695731+sincerelyindra@users.noreply.github.com>
2026-04-08 03:26:26 +0530

    Add package init for robust_pricing

---
+++ src/robust_pricing/__init__.py
@@ -0,0 +1,13 @@
+from .core import AmbiguitySet, MODELS, gamma_bar, gamma_bar_inv, gbar
+from .baselines import fit_linear, fit_quadratic, fit_logit
+
+__all__ = [
+    'AmbiguitySet',
+    'MODELS',
+    'gamma_bar',
+    'gamma_bar_inv',
+    'gbar',
+    'fit_linear',
+    'fit_quadratic',
+    'fit_logit',
+]
